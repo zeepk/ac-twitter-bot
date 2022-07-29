41c35618f75bba4ad2d346f61a96edbcb96b29b9
Matt Hughes <matthew.hughes@mainstreet.com>
2022-07-28 19:31:12 -0700

    init

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+const { TwitterApi } = require('twitter-api-v2');
+dotenv.config();
+
+const twitterClient = new TwitterApi({
+    appKey: process.env.CONSUMER_KEY ?? '',
+    appSecret: process.env.CONSUMER_SECRET ?? '',
+    accessToken: process.env.ACCESS_TOKEN ?? '',
+    accessSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
+});
+
+const client = twitterClient.readWrite;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Well done!");
+app.get('/', async (req, res) => {
+    await client.v2.tweet('Hello, this is a test.');
+    res.send('success');
 });
 
 app.listen(3000, () => {
-  console.log("The application is listening on port 3000!");
+    console.log('The application is listening on port 3000!');
 });
