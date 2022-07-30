008dac230097c2b03907e07b9ab9e19b9bd86917
Matt Hughes <mhughes.ipod@gmail.com>
2022-07-29 17:17:38 -0700

    image

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import json from './items.json';
-const { TwitterApi } = require('twitter-api-v2');
+import { TwitterApi } from 'twitter-api-v2';
+import fs from 'fs';
+import client from 'https';
 dotenv.config();
+
+const downloadImage = (url: string, filepath: string) => {
+    return new Promise((resolve, reject) => {
+        client.get(url, res => {
+            if (res.statusCode === 200) {
+                res.pipe(fs.createWriteStream(filepath))
+                    .on('error', reject)
+                    .once('close', () => resolve(filepath));
+            } else {
+                res.resume();
+                reject(
+                    new Error(
+                        `Request Failed With a Status Code: ${res.statusCode}`
+                    )
+                );
+            }
+        });
+    });
+};
 
 const twitterClient = new TwitterApi({
     appKey: process.env.CONSUMER_KEY ?? '',
@@ -11,23 +32,39 @@ const twitterClient = new TwitterApi({
     accessSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
 });
 
-const client = twitterClient.readWrite;
+const tweetClient = twitterClient.readWrite;
 
 const app = express();
 
 app.get('/', async (req, res) => {
+    const resp = {
+        status: 'error',
+        message: '',
+        data: {},
+    };
     const list: any[] = Array.of(json);
     const items = list[0];
     const itemIndex = Math.floor(Math.random() * items.length);
     const item = items[itemIndex];
     if (!item || !item.variants) {
-        res.send('No item found');
+        resp.message = 'No item found';
+        res.json(resp);
     }
+    const name = item.name.charAt(0).toUpperCase() + item.name.slice(1);
     const variant =
         item.variants[Math.floor(Math.random() * item.variants.length)];
     const imageUri = variant.closetImage ?? variant.storageImage;
-    await client.v2.tweet(item.name);
-    res.send('success');
+    const img = await downloadImage(imageUri, './image.png');
+    const upload = await tweetClient.v1.uploadMedia('./image.png');
+    await tweetClient.v2.tweet(name, {
+        media: {
+            media_ids: [upload],
+        },
+    });
+    resp.status = 'success';
+    resp.data = { name, imageUri, img, upload };
+
+    res.json(resp);
 });
 
 const port = process.env.PORT || 3000;
